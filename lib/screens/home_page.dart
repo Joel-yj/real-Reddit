df28e51959ab1bfde9470605039c6fe08afe3724
Joel <nyjc39sc.joel@gmail.com>
2022-11-12 14:01:50 +0800

    clean

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -5,42 +5,17 @@ import 'package:pointycastle/api.dart' as crypto;
 import 'package:real_reddit/utils/dependency_provider.dart';
 import 'package:real_reddit/utils/rsa_key_helper.dart';
 
-// TextStyle get whiteTextStyle => TextStyle(color: Colors.white);
+TextStyle get whiteTextStyle => TextStyle(color: Colors.white);
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({key, required this.title}) : super(key: key);
   final String title;
 
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
+  @override
+   _MyHomePageState createState() => _MyHomePageState();
+ }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // /// The Future that will show the Pem String
-  // late Future<String> futureText;
-  //
-  // /// Future to hold the reference to the KeyPair generated with PointyCastle
-  // /// in order to extract the [crypto.PrivateKey] and [crypto.PublicKey]
-  // late crypto.AsymmetricKeyPair<crypto.PublicKey, crypto.PrivateKey>
-  //     futureKeyPair;
-  //
-  // /// The current [crypto.AsymmetricKeyPair]
-  // //crypto.AsymmetricKeyPair keyPair = getKeyPair();
-  //
-  // /// With the helper [RsaKeyHelper] this method generates a
-  // /// new [crypto.AsymmetricKeyPair<crypto.PublicKey, crypto.PrivateKey>
-  // Future<crypto.AsymmetricKeyPair<crypto.PublicKey, crypto.PrivateKey>>
-  //     getKeyPair() {
-  //   var keyHelper = DependencyProvider.of(context).getRsaKeyHelper();
-  //   return keyHelper.computeRSAKeyPair(keyHelper.getSecureRandom());
-  // }
-  //
-  // /// GlobalKey to be used when showing the [Snackbar] for the successful
-  // /// copy of the Key
-  // final key = GlobalKey<ScaffoldState>();
-  //
-  // /// Text Editing Controller to retrieve the text to sign
-  // final TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -94,32 +69,6 @@ class _MyHomePageState extends State<MyHomePage> {
           )
         ],
       ),
-
-      // body: Center(
-      //   child: Padding(
-      //     padding: const EdgeInsets.all(8.0),
-      //     child: Column(
-      //       crossAxisAlignment: CrossAxisAlignment.stretch,
-      //       children: <Widget>[
-      //         MaterialButton(
-      //           color: Theme.of(context).colorScheme.secondary,
-      //           child: Text(
-      //             "Generate new Key Pair",
-      //             style: whiteTextStyle,
-      //           ),
-      //           onPressed: () {
-      //             setState(() {
-      //               // If there are any pemString being shown, then show an empty message
-      //               futureText = Future.value("");
-      //               // Generate a new keypair
-      //               // TODO : generate keypair here
-      //             });
-      //           },
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // ),
     );
   }
 }
