2e6f06a0263ed7e5b259212f69d109e99bb0ddb7
curseEmperor <jcjs1510@gmail.com>
2022-11-16 01:23:00 +0800

    after disc: split function

--- lib/objects/certificate.dart
+++ lib/objects/certificate.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:html';
 
+import 'package:basic_utils/basic_utils.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
 import 'package:localstorage/localstorage.dart';
@@ -16,6 +17,9 @@ class CertificateTemplate {
 
   var rsaHelper = RsaKeyHelper();
   var db = FirebaseFirestore.instance;
+  var storage = LocalStorage("UsersKey");
+
+  late RSAPrivateKey subPriKey;
 
   // Constructor
   CertificateTemplate(
@@ -51,11 +55,114 @@ class CertificateTemplate {
     );
   }
 
+  void step1() {
+    receiveBy = "gogo";
+    issueBy = "more-test";
+
+    // generate key pair
+    var res = rsaHelper.getRsaKeyPair(rsaHelper.getSecureRandom());
+    subPriKey = res.privateKey as RSAPrivateKey;
+
+    // public key into the cert template
+    subPubKey = res.publicKey as RSAPublicKey;
+  }
+
+  Future<void> step2() async {
+    // sign; gernerate encrypted msg
+
+    // make a string: "${new participant} belongs to ${lesson}"
+    message = "$issueBy signs for $receiveBy";
+
+    // get {lesson} private key and sign
+    var signerPrikey = db
+        .collection("Users") //lesson
+        .doc(issueBy)
+        .get()
+        .then((DocumentSnapshot doc) {
+      final data = doc.data() as Map<String, dynamic>;
+
+      // retrive signers private key
+      RSAPrivateKey rebuildKey = RSAPrivateKey(
+          BigInt.parse(data["Modulus"] as String),
+          BigInt.parse(data["PrivateKey"] as String),
+          BigInt.parse(data["p"] as String),
+          BigInt.parse(data["q"] as String));
+
+      return rebuildKey;
+    });
+
+    // Getting Private Key from Local Storage http://localhost:8000/#/
+    // await storage.ready;
+    // var out = await storage.getItem("$receiveBy-$issueBy");
+    // RSAPrivateKey shouldbe = CryptoUtils.rsaPrivateKeyFromPem(out);
+
+    // encrypt message for verification
+    encryptedMsgBytes = rsaHelper.rsaSign(await signerPrikey, message!);
+    print("in function: $encryptedMsgBytes");
+  }
+
+  void step3push() {
+    // -------push to store private key
+    var subPriJson = {
+      "PrivateKey": subPriKey.privateExponent.toString(),
+      "Modulus": subPriKey.modulus.toString(),
+      "p": subPriKey.p.toString(),
+      "q": subPriKey.q.toString(),
+    };
+
+    db
+        .collection("Users/$receiveBy/PrivateKeyCollection")
+        .doc(issueBy)
+        .get()
+        .then((doc) async {
+      if (!doc.exists) {
+        // put into db if dont have key yet
+        db
+            .collection("Users/$receiveBy/PrivateKeyCollection")
+            .doc(issueBy)
+            .set(subPriJson, SetOptions(merge: true));
+
+        // store to Local Storage of http://localhost:8000/#/
+        var priKeyPEM = CryptoUtils.encodeRSAPrivateKeyToPem(subPriKey);
+        await storage.ready;
+        storage.setItem("$receiveBy-$issueBy", priKeyPEM); // store as PEM
+        // print(await storage.getItem("$receiveBy-$issueBy"));
+      } else {
+        // got key for tat class
+        // TODO: refresh only if invalid
+        print("already exist");
+      }
+    });
+    // -----------end of storing private Key
+    print(receiveBy);
+    print(issueBy);
+    //TODO: only store cert if uniqued
+    db
+        .collection("Users/$receiveBy/Certificates")
+        .where("IssueBy", isEqualTo: issueBy)
+        .where("ReceiveBy", isEqualTo: receiveBy)
+        .get()
+        .then((res) {
+      if (res.docs.isEmpty) {
+        print("in storing");
+        // Store certificate
+        var certJson = toJson();
+        db
+            .collection("Users/$receiveBy/Certificates")
+            .doc()
+            .set(certJson, SetOptions(merge: true));
+      } else {
+        // TODO: refresh cert?? only if invalid
+        print("HAVE: $issueBy give to $receiveBy");
+      }
+    });
+  }
+
 //---------------processes-----------
   Future<void> request() async {
     // *NEW* get both issue and receive by HERE
     receiveBy = "gogo";
-    issueBy = "CE3003";
+    issueBy = "CE33";
 
     // generate key pair
     var res = rsaHelper.getRsaKeyPair(rsaHelper.getSecureRandom());
@@ -78,7 +185,7 @@ class CertificateTemplate {
         .collection("Users/$receiveBy/PrivateKeyCollection")
         .doc(issueBy)
         .get()
-        .then((doc) {
+        .then((doc) async {
       if (!doc.exists) {
         // put into db if dont have key yet
         db
@@ -86,10 +193,10 @@ class CertificateTemplate {
             .doc(issueBy)
             .set(subPriJson, SetOptions(merge: true));
 
-        // TODO: store to local
-        // var storage = LocalStorage("UsersKey");
-        // await storage.ready;
-        // storage.setItem("$receiveBy-$issueBy", subPriJson);
+        // store to Local Storage of http://localhost:8000/#/
+        var priKeyPEM = CryptoUtils.encodeRSAPrivateKeyToPem(subPriKey);
+        await storage.ready;
+        storage.setItem("$receiveBy-$issueBy", priKeyPEM); // store as PEM
         // print(await storage.getItem("$receiveBy-$issueBy"));
       } else {
         // got key for tat class
@@ -123,7 +230,7 @@ class CertificateTemplate {
   //TODO: #JOEL# sign(String lesson) - input grp name
   Future<void> sign() async {
     // make a string: "${new participant} belongs to ${lesson}"
-    message = "$issueBy signs for chow";
+    message = "$issueBy signs for $receiveBy";
 
     // get {lesson} private key and sign
     var signerPrikey = db
@@ -142,6 +249,11 @@ class CertificateTemplate {
 
       return rebuildKey;
     });
+
+    // Getting Private Key from Local Storage http://localhost:8000/#/
+    await storage.ready;
+    var out = await storage.getItem("$receiveBy-$issueBy");
+    RSAPrivateKey shouldbe = CryptoUtils.rsaPrivateKeyFromPem(out);
 
     // encrypt message for verification
     encryptedMsgBytes = rsaHelper.rsaSign(await signerPrikey, message!);
