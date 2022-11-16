6ff55697cf0aff3eadda75fdbea6e3a6635e1299
Joel <nyjc39sc.joel@gmail.com>
2022-11-16 11:49:51 +0800

    Update cert_check_helper.dart

--- lib/utils/cert_check_helper.dart
+++ lib/utils/cert_check_helper.dart
@@ -14,7 +14,7 @@ class CertCheckHelper {
   // 1) get trusted source pub key
   // 2) dumb out all cert of users in certificate, manage with data struct
   // 3) go thru certs in a chain to check validity
-  Future<bool> checking() async {
+  Future<bool> checking(String oldUser, String curUser) async {
     String oldUser = "chow"; // TODO:user is chow joins test-class1
     String newUser = "jason";
     List<CertificateTemplate> listOfCert = [];
